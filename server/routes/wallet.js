53fe96abef8961af1944945f8b5da46dcda18bd7
Lee Adcock <lee@katieandlee.com>
2015-07-10 18:29:30 +0000

    updating lee's work

--- server/routes/wallet.js
+++ server/routes/wallet.js
@@ -5,6 +5,32 @@ var bitcoin = require('bitcoinjs-lib');
 module.exports = function(router) {
 
     router.route('/user/:user_id/wallet')
+    .post(function(req, res) {
+
+
+        User.findOne({'id':req.params.user_id}, function(err, user) {
+            if (err)
+                res.send(err);
+            Wallet.findById(user.wallet, function(err, wallet) {
+                var chain = require('chain-wallets-node');
+                var apiClient = wallet.getClient();
+                
+                
+                
+                apiClient.issueAsset('AVSEVsuMxtJd4g5AQLxN3fNFgDrLiA2NRy', 
+                [{
+                    address:'181So6gyYurnM69o5KjMXotZE8CeVK45YV',
+                    amount:1
+                }]
+                , function(stuff) {
+                    console.log("transfered "+stuff);
+                });
+                
+            });
+        });
+
+
+    })
     .get(function(req, res) {
         User.findOne({'id':req.params.user_id}, function(err, user) {
             if (err)
